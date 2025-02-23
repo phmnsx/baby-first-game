2c1af4b8eedefe7ccac6018fe6e8b1fa32f8767f
phmnsx <lynnwastinghertime@gmail.com>
2025-02-23 01:09:03 -0300

    Advances in horizontal movement

--- Pessoar.cs
+++ Pessoar.cs
@@ -7,8 +7,14 @@ public class Pessoar : Node2D
 	// private int a = 2;
 	// private string b = "text";
 	private Vector2 MovementDirection = Vector2.Zero;
-	private float MovementSpeed = 100;
+	private const float DEFAULT_MOVEMENT_SPEED = 100;
+	public static float MaxMovementSpeed = 100;
+	private float Speed = 100;
+	private float Acceleration = 20;
+	private float Friction = MaxMovementSpeed/24;
+	
 	// Called when the node enters the scene tree for the first time.
+	
 	public override void _Ready()
 	{
 		GD.Print("Oiiii :3");
@@ -18,10 +24,38 @@ public class Pessoar : Node2D
   public override void _Process(float delta)
   {
 	MovementDirection = Vector2.Zero;
-	if (Input.IsActionPressed("Up")) { MovementDirection += new Vector2(0,-1); }
-	if (Input.IsActionPressed("Down")) { MovementDirection += new Vector2(0,1); }
+	
+	if (Input.IsActionPressed("Up")) {  }
+	if (Input.IsActionPressed("Down")) {  }
 	if (Input.IsActionPressed("Left")) { MovementDirection += new Vector2(-1,0); }
 	if (Input.IsActionPressed("Right")) { MovementDirection += new Vector2(1,0); }
-	Position += MovementDirection.Normalized()*MovementSpeed*delta;
+	
+	if (MovementDirection == Vector2.Zero)
+	{
+		ReduceAcceleration();
+	}
+	else if (Math.Abs(Speed) < MaxMovementSpeed)
+	{
+		Speed += Acceleration*MovementDirection.x;
+	}
+	
+	CorrectSpeed();
+	GD.Print(Speed);
+	GD.Print(Friction);
+	Position += new Vector2(1,0)*Speed*delta;
+  }
+
+  private void CorrectSpeed()
+  {
+	if (Math.Abs(Speed) > MaxMovementSpeed)
+  	{ Speed += (-1)*Friction*MovementDirection.x ; }
+  }
+  private void ReduceAcceleration()
+  {
+  	if (Speed == 0) { return; }
+  	if (Speed > 0) { Speed -= Friction; }
+	if (Speed < 0) { Speed += Friction; }
+	if (Math.Abs(Speed) < Friction) { Speed = 0; }
+	
   }
 }
